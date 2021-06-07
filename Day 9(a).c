eed280bcc5aa69bd5dbd38d1903ea22118f831f8
Achyuthr21 <83939199+Achyuthr21@users.noreply.github.com>
2021-06-07 21:53:38 +0530

    Create Day 9(a).c

---
+++ Day 9(a).c
@@ -0,0 +1,28 @@
+C program to print the HCF of two numbers
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+     int a, b, i, hcf;
+     scanf("%d %d", &a, &b);
+     for(i=1;(i<=a)&&(i<=b); ++i)
+    {
+       if((a%i==0)&&(b%i==0))
+       hcf= i;
+    }
+    for(i=1;(i<=a)&&(i<=b); ++i)
+    {
+       if((a%i==0)&&(b%i==0))
+       hcf= i;
+    }
+    printf("%d",hcf);
+
+    return 0;
+}
+   
+    
+
